b9541f59295a34223861b53f34cb97918eef06ae
Sayan Chatterjee <productionssc69@gmail.com>
2024-01-23 18:17:26 +0530

    Edits done

--- cyphervaultapp/src/App.js
+++ cyphervaultapp/src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
+import { FaUser, FaLock } from "react-icons/fa6";
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div className="wrapper">
+      <form action="">
+        <h1>Login Form</h1>
+        <div className="input-box">
+          <input type="text" placeholder="Username" required /><FaUser className='icon' />
+
+        </div>
+        <div className="input-box">
+          <input type="password" placeholder="Password" required /><FaLock className='icon' />
+
+        </div>
+
+        <div className="remember-forgot">
+          <label><input type="checkbox" />Remember Me</label>
+          <a href="/">Forgot password?</a>
+        </div>
+
+        <button type="submit">Login</button>
+
+        <div className="register-link">
+          <p>Don't have an account? <a href="/">Register</a></p>
+        </div>
+      </form>
     </div>
+    </>
   );
 }
 
